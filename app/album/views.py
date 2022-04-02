5d4f3936273e5d14f31d681f7f076b57f5b21abc
Jacob Tavener <jacob.tavener@gmail.com>
2022-04-02 15:17:25 +0100

    refactor views to use common base class

--- app/album/views.py
+++ app/album/views.py
@@ -7,14 +7,12 @@ from core.models import Tag, Artist
 from album import serializers
 
 
-class TagViewSet(viewsets.GenericViewSet,
-                 mixins.ListModelMixin,
-                 mixins.CreateModelMixin):
-    """Manage tags in the database"""
+class BaseAlbumAttrViewSet(viewsets.GenericViewSet,
+                           mixins.ListModelMixin,
+                           mixins.CreateModelMixin):
+    """Base viewset for user owned album attributes"""
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
-    queryset = Tag.objects.all()
-    serializer_class = serializers.TagSerializer
 
     def get_queryset(self):
         """Return objects for the current authenticated user only"""
@@ -25,20 +23,13 @@ class TagViewSet(viewsets.GenericViewSet,
         serializer.save(user=self.request.user)
 
 
-class ArtistViewSet(viewsets.GenericViewSet,
-                    mixins.ListModelMixin,
-                    mixins.CreateModelMixin):
-    """Manage artists in the database"""
+class TagViewSet(BaseAlbumAttrViewSet):
+    """Manage tags in the database"""
+    queryset = Tag.objects.all()
+    serializer_class = serializers.TagSerializer
 
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
+
+class ArtistViewSet(BaseAlbumAttrViewSet):
+    """Manage artists in the database"""
     queryset = Artist.objects.all()
     serializer_class = serializers.ArtistSerializer
-
-    def get_queryset(self):
-        """Return objects for the current authenticated user only"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
-
-    def perform_create(self, serializer):
-        """Create a new artist"""
-        serializer.save(user=self.request.user)
